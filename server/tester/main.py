bca05b4c1077d5d71db4a9681b13b914eb45987b
JessicaBoisserand <boisserand.jessica@orange.fr>
2022-04-09 17:48:00 +0200

    feat: add funct to test nginx

--- server/tester/main.py
+++ server/tester/main.py
@@ -1,19 +1,47 @@
 from telnetlib import Telnet
 import sys
 
-if (len(sys.argv) != 3):
-    print("error: No port secified. Try \"python3 main.py <PORT> <File REQUEST>\".")
+error = False
+if (len(sys.argv) != 3 and len(sys.argv) != 5):
+    error = True
+elif (len(sys.argv) == 5 and sys.argv[3] != "-nginx"):
+    error = True
+if (error):
+    print("error: Port Not secified.")
+    print("try \" python3 main.py <PORT> <File REQUEST> [OPT] -nginx <PORT>\".")
     exit (1)
 
-ip="localhost"
-t = Telnet()
-t.open(ip, int(sys.argv[1]))
+def test_webserv(port, request):
+    ip="localhost"
+    t = Telnet()
+    t.open(ip, int(port))
 
-with open(sys.argv[2], 'r') as f:
-    request = f.read().replace('\n', '\r\n')
+    with open(sys.argv[2], 'r') as f:
+        request = f.read().replace('\n', '\r\n')
 
-t.write(bytes(request, 'ascii'))
-response = t.read_until(b'</html>\r\n', 10) # need to replace this 
-print(response.decode())
+    t.write(bytes(request, 'ascii'))
+    response = t.read_until(b'</html>\r\n', 10) # need to replace this 
+    return response.decode()
 
-t.close()
+    t.close()
+
+def test_nginx(port):
+    ip="localhost"
+    t = Telnet()
+    t.open(ip, int(port))
+
+    with open(sys.argv[2], 'r') as f:
+        request = f.read().replace('\n', '\r\n')
+
+    t.write(bytes(request, 'ascii'))
+    response = t.read_until(b'</html>\r\n', 10) # need to replace this 
+    return response.decode()
+
+    t.close()
+
+print("./webserv ------------------------------------------------------------")
+r_webserv = test_webserv(sys.argv[1], sys.argv[2])
+print(r_webserv)
+print("./nginx ------------------------------------------------------------")
+r_nginx = test_nginx(sys.argv[4])
+print(r_nginx)
